97a075c069df03ccc372a48605830cd0edca9c11
HenriqueContini <henrique.coutine@gmail.com>
2023-09-07 23:26:42 -0300

    Feat: products table

--- frontEnd/src/components/ProductsTable/index.tsx
+++ frontEnd/src/components/ProductsTable/index.tsx
@@ -6,40 +6,78 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import ExpandableTableRow from "./ExpandableTableRow";
+import { Box, Stack, Typography, useTheme } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import WarningIcon from "@mui/icons-material/Warning";
 
 interface ProductsTableProps {
   products: ProductType[];
 }
 
 export default function ProductsTable({ products }: ProductsTableProps) {
+  const theme = useTheme();
+
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 550 }} size="small" aria-label="Product Table">
+      <Table sx={{ minWidth: 300 }} size="small" aria-label="Product Table">
         <TableHead>
           <TableRow>
+            <TableCell padding="checkbox" />
             <TableCell>Produto</TableCell>
             <TableCell align="right">Código</TableCell>
             <TableCell align="right">Preço atual</TableCell>
             <TableCell align="right">Preço novo</TableCell>
-            <TableCell align="right">Problemas</TableCell>
+            <TableCell align="right">Situação</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {products.map((item) => (
-            <TableRow
-              key={item.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+          {products.map((product) => (
+            <ExpandableTableRow
+              key={product.code}
+              expandComponent={
+                <TableCell colSpan={5}>
+                  <Typography variant="subtitle2" component="p" margin="10px">
+                    {product.problems && product.problems.length > 0
+                      ? "Problemas:"
+                      : "Este produto não possui nenhum problema!"}
+                  </Typography>
+
+                  <Stack spacing={1}>
+                    {product.problems &&
+                      product.problems.map((item, index) => (
+                        <Box
+                          sx={{ padding: "5px 10px" }}
+                          component={Paper}
+                          variant="outlined"
+                          key={index}
+                        >
+                          {item}
+                        </Box>
+                      ))}
+                  </Stack>
+                </TableCell>
+              }
             >
               <TableCell component="th" scope="row">
-                {item.name}
+                {product.name}
               </TableCell>
-              <TableCell align="right">{item.code}</TableCell>
-              <TableCell align="right">{item.sales_price}</TableCell>
-              <TableCell align="right">{item.new_price}</TableCell>
+              <TableCell align="right">{product.code}</TableCell>
               <TableCell align="right">
-                {item.problems?.map((problem) => problem)}
+                R$ {Number(product.sales_price).toFixed(2)}
               </TableCell>
-            </TableRow>
+              <TableCell align="right">
+                R$
+                {product.new_price ? Number(product.new_price).toFixed(2) : ""}
+              </TableCell>
+              <TableCell align="right">
+                {product.problems && product.problems.length > 0 ? (
+                  <WarningIcon sx={{ color: theme.palette.warning.main }} />
+                ) : (
+                  <CheckCircleIcon sx={{ color: theme.palette.success.dark }} />
+                )}
+              </TableCell>
+            </ExpandableTableRow>
           ))}
         </TableBody>
       </Table>
--- frontEnd/src/pages/Home/index.tsx
+++ frontEnd/src/pages/Home/index.tsx
@@ -3,26 +3,25 @@ import FileForm from "../../components/FileForm";
 import ProductsTable from "../../components/ProductsTable";
 import { checkDataAPI } from "../../services/checkData";
 import { ApiResponseType } from "../../types/apiResponse";
-import { CSVType } from "../../types/csvType";
 import { ProductType } from "../../types/productType";
 import { useState } from "react";
 
 export default function Home() {
   const [file, setFile] = useState<File>();
   const [products, setProducts] = useState<ProductType[]>([]);
-  const [csv, setCSV] = useState<CSVType[]>([]);
+  /*   const [csv, setCSV] = useState<CSVType[]>([]); */
 
   const checkData = async () => {
     if (file) {
       const data: ApiResponseType = await checkDataAPI(file);
 
       if (data.products) setProducts(data.products);
-      if (data.csvProblems) setCSV(data.csvProblems);
+      /* if (data.csvProblems) setCSV(data.csvProblems); */
     }
   };
 
   return (
-    <Container>
+    <Container sx={{ display: "flex", flexDirection: "column", gap: "40px" }}>
       <FileForm file={file} setFile={setFile} checkData={checkData} />
       <ProductsTable products={products} />
     </Container>
